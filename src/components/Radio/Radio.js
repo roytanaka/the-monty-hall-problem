28d85f6828bd3a0d06eeb0d984c3c8f6e518ec73
Roy Tanaka <tanaka@mac.com>
2021-04-12 19:22:58 -0400

    radio hover selected state

--- src/components/Radio/Radio.js
+++ src/components/Radio/Radio.js
@@ -23,7 +23,6 @@ const Radio = props => {
       <path d="M12,20C7.58,20 4,16.42 4,12C4,7.58 7.58,4 12,4C16.42,4 20,7.58 20,12C20,16.42 16.42,20 12,20M12,2C6.48,2 2,6.48 2,12C2,17.52 6.48,22 12,22C17.52,22 22,17.52 22,12C22,6.48 17.52,2 12,2M12,7C9.24,7 7,9.24 7,12C7,14.76 9.24,17 12,17C14.76,17 17,14.76 17,12C17,9.24 14.76,7 12,7Z"></path>
     </svg>
   );
-
   return (
     <label className={styles.Radio}>
       <span className={styles.Radio__input}>
@@ -39,7 +38,14 @@ const Radio = props => {
           {props.isSelected ? checked : unchecked}
         </span>
       </span>
-      <span className={styles.Radio__label}>{props.children}</span>
+      <span
+        className={[
+          styles.Radio__label,
+          props.isSelected ? styles.selected : '',
+        ].join(' ')}
+      >
+        {props.children}
+      </span>
     </label>
   );
 };
